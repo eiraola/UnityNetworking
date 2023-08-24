3a2d2135376740e3878f244ff0f699aa9c022e41
Eduardo Montero <emontero.iraola@gmail.com>
2023-08-24 21:57:37 +0200

    main

--- Assets/_UPPN01/Scripts/UI/Leaderboard/Leaderboard.cs
+++ Assets/_UPPN01/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Leaderboard : MonoBehaviour
+public class Leaderboard : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private Transform leaderboardEntityHolder;
+    [SerializeField]
+    private LeaderboardEntry leaderBoardEntryPrefab;
+    NetworkList<>
 }
---
+++ Assets/_UPPN01/Scripts/UI/Leaderboard/LeaderboardEntryState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public struct LeaderboardEntryState {
+
+    public ulong ClientId;
+    public FixedString32Bytes PlayerName;
+}
